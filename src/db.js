dfde6c0ac8e3f0e4211494ac27ab4184b5253236
LOctavioDev <octaviodevtech@gmail.com>
2024-04-29 02:17:04 -0600

    Fix: URLDB

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const conectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.DB_URL || `mongodb+srv://octadev:112020a@clusteroctavio.n8a1lsl.mongodb.net/merndb?retryWrites=true&w=majority&appName=ClusterOctavio`);
     console.log(`>>>DATABASE SUCCESSFULLY CONNECTED`);
   } catch (error) {
     console.error(error);
